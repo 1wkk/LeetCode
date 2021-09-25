8e6e3efee5c499b67d9651b3dec2cfe6a640035f
ryanmoyo <iherewithmyheart@gmail.com>
2021-09-25 11:03:43 +0800

    📝 update - p - 583 & 1143

---
+++ typescript/problems/1143.ts
@@ -0,0 +1,15 @@
+function longestCommonSubsequence(text1: string, text2: string): number {
+  const len1 = text1.length
+  const len2 = text2.length
+  const dp = new Array(len1 + 1).fill(null).map(() => new Array(len2 + 1).fill(0))
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[len1][len2]
+}
