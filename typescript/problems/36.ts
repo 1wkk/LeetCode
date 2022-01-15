07631b8563dc14a15079ca9339e45a22132c3b75
ryanmoyo <iherewithmyheart@gmail.com>
2022-01-15 15:32:41 +0800

    🎨 prettier format

--- typescript/problems/36.ts
+++ typescript/problems/36.ts
@@ -5,9 +5,7 @@ const ASCII_ZERO = getAscii('0')
 function isValidSudoku(board: string[][]): boolean {
   const rows = new Array(9).fill(0).map(() => new Array(9).fill(0))
   const cols = new Array(9).fill(0).map(() => new Array(9).fill(0))
-  const boxes = new Array(3)
-    .fill(0)
-    .map(() => new Array(3).fill(0).map(() => new Array(9).fill(0)))
+  const boxes = new Array(3).fill(0).map(() => new Array(3).fill(0).map(() => new Array(9).fill(0)))
 
   const row = board.length
   const col = board[0].length
@@ -16,11 +14,7 @@ function isValidSudoku(board: string[][]): boolean {
       const c = board[i][j]
       if (c !== '.') {
         const idx = getAscii(c) - ASCII_ZERO - 1
-        if (
-          ++rows[i][idx] > 1 ||
-          ++cols[j][idx] > 1 ||
-          ++boxes[~~(i / 3)][~~(j / 3)][idx] > 1
-        ) {
+        if (++rows[i][idx] > 1 || ++cols[j][idx] > 1 || ++boxes[~~(i / 3)][~~(j / 3)][idx] > 1) {
           return false
         }
       }
