79b2e81633bb34ef3c57edbb5a48c6a581ec5b04
ryanmoyo <iherewithmyheart@gmail.com>
2022-01-25 12:11:35 +0800

    🚨 daily 1688 count-of-matches-in-tournament

---
+++ leetcode/all/python3/1688.比赛中的配对次数.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=1688 lang=python3
+#
+# [1688] 比赛中的配对次数
+#
+
+# @lc code=start
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
+
+
+# @lc code=end
---
+++ leetcode/all/typescript/1688.比赛中的配对次数.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=1688 lang=typescript
+ *
+ * [1688] 比赛中的配对次数
+ */
+
+// @lc code=start
+function numberOfMatches(n: number): number {
+  return n - 1
+}
+// @lc code=end
